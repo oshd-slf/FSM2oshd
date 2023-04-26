d3f16c5b2a58515e478ac511441063c012f93081
jannefiluren <magnusson@slf.ch>
2023-04-26 10:54:33 +0200

    Rearrange code into core and applications folders

--- compil.bat
+++ compil.bat
@@ -5,17 +5,18 @@ echo off
 set optim=-O3
 
 echo on
-set mods= MODULES_TXT.F90 MODE_WRITE.F90
-set routines= LWRADTOPO.F90 SWRADTOPO.F90 CANOPY.F90 CUMULATE.F90 DRIVE_TXT.F90 DUMP_TXT.F90 EBALFOR.F90 ^
-EBALSRF_SBG.F90 EBALSRF.F90 FSM2.F90 LUDCMP.F90 OPEN_FILES.F90 PHYSICS_TXT.F90 QSAT.F90 ^
-RADIATION.F90 SETUP_TXT.F90 SNOW.F90 SNOWCOVERFRACTION.F90 SOIL.F90 ^
-SFEXCH.F90 THERMAL.F90 TRIDIAG.F90 OUTPUT.F90
+set mods= txt/MODULES.F90 txt/MODE_WRITE.F90
+set routines= core/LWRADTOPO.F90 core/SWRADTOPO.F90 core/CANOPY.F90 core/EBALFOR.F90 ^
+core/EBALSRF_SBG.F90 core/EBALSRF.F90 core/FSM2.F90 core/LUDCMP.F90 core/QSAT.F90 ^
+core/RADIATION.F90 core/SNOW.F90 core/SNOWCOVERFRACTION.F90 core/SOIL.F90 core/SFEXCH.F90 ^
+core/THERMAL.F90 core/TRIDIAG.F90 ^
+txt/OPEN_FILES.F90 txt/CUMULATE.F90 txt/DRIVE.F90 txt/DUMP.F90 txt/PHYSICS.F90 txt/SETUP.F90 txt/OUTPUT.F90
 if /I "%1" == "-O3" set optim= %1
 if /I "%1" == "-O2" set optim= %1
 if /I "%1" == "-O1" set optim= %1
 if /I "%1" == "-O0" set optim= %1
 set profil= %2
-cd fsm_txt_32
+cd src
 gfortran %mods% %routines% %optim% %profil% -cpp -ffpe-trap=overflow -o FSMOSHD
 del *.mod
 move FSMOSHD.exe ..\FSMOSHD.exe
