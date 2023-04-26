fe3b0a2bdb208e9b70ed1310fb0f5f208274ea0d
jannefiluren <magnusson@slf.ch>
2023-04-26 12:37:42 +0200

    Update compile script to latest core refactorizations

--- compil.bat
+++ compil.bat
@@ -5,12 +5,12 @@ echo off
 set optim=-O3
 
 echo on
-set mods= txt/MODULES.F90 txt/MODE_WRITE.F90
+set mods= core/MODULES.F90 txt/MODULES.F90
 set routines= core/LWRADTOPO.F90 core/SWRADTOPO.F90 core/CANOPY.F90 core/EBALFOR.F90 ^
 core/EBALSRF_SBG.F90 core/EBALSRF.F90 core/FSM2.F90 core/LUDCMP.F90 core/QSAT.F90 ^
 core/RADIATION.F90 core/SNOW.F90 core/SNOWCOVERFRACTION.F90 core/SOIL.F90 core/SFEXCH.F90 ^
-core/THERMAL.F90 core/TRIDIAG.F90 ^
-txt/OPEN_FILES.F90 txt/CUMULATE.F90 txt/DRIVE.F90 txt/DUMP.F90 txt/PHYSICS.F90 txt/SETUP.F90 txt/OUTPUT.F90
+core/THERMAL.F90 core/TRIDIAG.F90 core/PHYSICS.F90 ^
+txt/OPEN_FILES.F90 txt/CUMULATE.F90 txt/DRIVE.F90 txt/DUMP.F90 txt/SETUP.F90 txt/OUTPUT.F90
 if /I "%1" == "-O3" set optim= %1
 if /I "%1" == "-O2" set optim= %1
 if /I "%1" == "-O1" set optim= %1
